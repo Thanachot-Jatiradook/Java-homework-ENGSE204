f3ff0fcd772dd1801ee6401c859aea0da63b5b34
Thanachot Jatiradook <jatiradook13@gmail.com>
2025-11-22 18:26:13 +0700

    lab10404

--- Lab104.java
+++ Lab104.java
@@ -1,14 +1,16 @@
-//No AI 100% code by Tanachot Jatiradook
 import java.util.Scanner;
 
 public class Lab104 {
-    public static void main ( String[] args ) {
+
+    public static void main () {
+
         Scanner scanner = new Scanner ( System.in ) ;
         System.out.println ( "Enter your score : " ) ;
         int score = scanner.nextInt();
 
         if ( score >= 80 ) {
             System.out.println ( "A" ) ;
+
         }   else if ( score >= 70 ) {
                 System.out.println ( "B" ) ;
             }
@@ -21,5 +23,7 @@ public class Lab104 {
             else if ( score <= 49 ) {
                 System.out.println ("F" ) ;
             }
+           
         }
+        
     }
